333147f865947bcfa546cd02126079edf624eca2
masterduck7 <luis.ramirez.12@sansano.usm.cl>
2020-08-26 20:04:42 -0400

    Fix tests

--- src/app/components/game/game.component.spec.ts
+++ src/app/components/game/game.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { GameComponent } from './game.component';
 
 describe('GameComponent', () => {
@@ -10,7 +11,8 @@ describe('GameComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ GameComponent ],
       imports: [
-        RouterTestingModule.withRoutes([])
+        RouterTestingModule.withRoutes([]),
+        TranslateModule.forRoot()
       ]
     })
     .compileComponents();
